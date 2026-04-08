63d83bc59768e7ccdf72c2054ef0de23249cea3e
GabrielCGeraldo <73504934+GabrielCGeraldo@users.noreply.github.com>
2026-04-08 23:28:27 +0000

    ✨ Aplicação da Raiz Quadrada

--- calculator.js
+++ calculator.js
@@ -11,6 +11,8 @@ function div(v1, v2) {}
 function mult(v1, v2) {}
 
 // retorna o valor da raiz quadrada do valor recebido por parâmetro
-function square(v1) {}
+function square(v1) {
+    return Math.pow(v, 2);
+}
 
 console.log(sum(v1, v2));
